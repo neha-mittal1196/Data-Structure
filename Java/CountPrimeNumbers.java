03ff30b8fa91185ad58fa1dd5312d0722e76f7d7
NehaSingla1196 <71941250+NehaSingla1196@users.noreply.github.com>
2023-08-06 17:11:49 +0530

    CountPrimeNumbers.java

---
+++ Java/CountPrimeNumbers.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int solve(int A) {
+        int primeFactorsCount = 0;
+        //Edge case- In case of 2, prime is 1.
+        if(A == 2)
+          return 1;
+
+      //Iterate from 2 till that number and check that number is prime or not. O(N*sqrt(N)
+        for(int i=2; i<=A; i++){
+            if(isPrime(i)== 1)
+               primeFactorsCount++;    
+        }
+
+         return primeFactorsCount;
+    }
+
+    int isPrime(int A){
+        if (A == 1)
+            return 0;
+
+        for (int i = 2; i*i <=A; i++) {
+            if (A % i == 0)
+                return 0;
+        }
+
+        return 1;
+    }
+}
