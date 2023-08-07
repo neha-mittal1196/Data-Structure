212a6a74dff584ca3c7e64eda0a83b3b610e4a4d
NehaSingla1196 <71941250+NehaSingla1196@users.noreply.github.com>
2023-08-07 15:25:59 +0530

    Update and rename CountFactors.java to 1_CountFactors.java

--- Java/1_CountFactors.java
+++ Java/1_CountFactors.java
@@ -1,3 +1,7 @@
+//Given an integer A, you need to find the count of it's factors.
+//Factor of a number is the number which divides it perfectly leaving no remainder.
+//Example : 1, 2, 3, 6 are factors of 6
+
 public class Solution {
     public int solve(int A) {
 
